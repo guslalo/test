6a208bc0b512ed6634f3f4f2cc53f6493b3644f8
guslalo <gf.renjifo@gmail.com>
2020-06-28 23:04:21 -0400

    components login, blcked, recovery, create

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,21 +1,29 @@
 //core
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
 
+//modules
 import { NgxSpinnerModule } from "ngx-spinner";
 
 //routing
 import { AuthRoutingModule } from './auth-routing.module';
 
+//components
+import { LoginComponent } from './components/login/login.component';
+import { RecoveryComponent } from './components/recovery/recovery.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { BlockedAccountComponent } from './components/blocked-account/blocked-account.component';
+
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RecoveryComponent,
+    CreateAccountComponent,
+    BlockedAccountComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/auth/components/blocked-account/blocked-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blocked-account',
+  templateUrl: './blocked-account.component.html',
+  styleUrls: ['./blocked-account.component.scss']
+})
+export class BlockedAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
