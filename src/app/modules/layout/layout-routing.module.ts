292d609f6c043cc1d57878a6375945abb2f94547
guslalo <gf.renjifo@gmail.com>
2020-07-08 10:55:53 -0400

    remove hash

--- src/app/modules/layout/layout-routing.module.ts
+++ src/app/modules/layout/layout-routing.module.ts
@@ -55,7 +55,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes , { useHash: true }),
+    RouterModule.forRoot(routes)//, { useHash: true }
   ],
   exports: [RouterModule]
 })
