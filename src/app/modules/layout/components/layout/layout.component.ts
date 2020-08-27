c36364632c514d17d6cf57ce4a39b8835403919c
Cristian Alarcon T <35607474+crisaty@users.noreply.github.com>
2020-08-26 20:37:52 -0400

    Fix bug in login for multiple clinic, same profile [choose-context]

--- src/app/modules/layout/components/layout/layout.component.ts
+++ src/app/modules/layout/components/layout/layout.component.ts
@@ -3,70 +3,64 @@ import { CurrentUserService } from '../../../../services/current-user.service';
 import { UserLogin } from '../../../../models/models';
 // import { slideInAnimation } from '../../../../shared/animations';
 import { SharedModule } from '../../../../shared/shared.module';
-import { trigger, state, style, animate, transition} from '@angular/animations';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   animations: [
-    trigger ('sideBarAnimation', [
-      state('open', style({
-        // backgroundColor: '#000000',
-        // transform: 'scale(1.5)'
-      })),
-      state('closed', style({
-        width: '68px',
-        padding: '0px'
-      })),
-      transition('closed => open', animate ('100ms ease-in')),
-      transition('open => closed', animate ('100ms ease-out'))
+    trigger('sideBarAnimation', [
+      state(
+        'open',
+        style({
+          // backgroundColor: '#000000',
+          // transform: 'scale(1.5)'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          width: '68px',
+          padding: '0px',
+        })
+      ),
+      transition('closed => open', animate('100ms ease-in')),
+      transition('open => closed', animate('100ms ease-out')),
     ]),
-    trigger ('sideBarElements', [
-      state('open', style({
-        // backgroundColor: '#000000',
-        // transform: 'scale(1.5)'
-      })),
-      state('closed', style({
-        display: 'none',
-      })),
-      transition('closed => open', animate ('100ms ease-in')),
-      transition('open => closed', animate ('100ms ease-out'))
+    trigger('sideBarElements', [
+      state(
+        'open',
+        style({
+          // backgroundColor: '#000000',
+          // transform: 'scale(1.5)'
+        })
+      ),
+      state(
+        'closed',
+        style({
+          display: 'none',
+        })
+      ),
+      transition('closed => open', animate('100ms ease-in')),
+      transition('open => closed', animate('100ms ease-out')),
     ]),
-   ]
+  ],
 })
 export class LayoutComponent implements OnInit {
+  currentUser: any = JSON.parse(localStorage.getItem('currentUser'));
 
-  constructor(public currentUser: CurrentUserService) {}
-  public UserLogin: UserLogin;
-  public user: any;
-  public userCurrent: any;
+  constructor() {}
   public state = 'open';
 
   status = false;
 
-  ngOnInit(): void {
-    console.log(this.currentUser);
-    // this.user = this.currentUser;
-    this.user = new UserLogin(
-      JSON.parse(localStorage.getItem('currentUser')).id,
-      JSON.parse(localStorage.getItem('currentUser')).email,
-      JSON.parse(localStorage.getItem('currentUser')).name,
-      JSON.parse(localStorage.getItem('currentUser')).lastName,
-      JSON.parse(localStorage.getItem('currentUser')).access_token,
-      JSON.parse(localStorage.getItem('currentUser')).expires_in,
-      JSON.parse(localStorage.getItem('currentUser')).internalCode,
-      JSON.parse(localStorage.getItem('currentUser')).administrativeData,
-      JSON.parse(localStorage.getItem('currentUser')).administrativeDataContext,
-      JSON.parse(localStorage.getItem('currentUser')).role
-    );
-    console.log(this.user); /**/
-  }
+  ngOnInit(): void {}
 
-  sideBar(){
+  sideBar() {
     this.state = this.state === 'open' ? 'closed' : 'open';
   }
-  sideBarMenu(){
+  sideBarMenu() {
     this.status = !this.status;
   }
 }
