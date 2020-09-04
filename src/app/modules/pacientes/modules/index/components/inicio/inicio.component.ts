d5be7b9dae137a082e8323df4d53a64e95585b79
Cristian Alarcon T <35607474+crisaty@users.noreply.github.com>
2020-09-04 12:18:55 -0400

    Remove current date on all user's home, add user's name welcome

--- src/app/modules/pacientes/modules/index/components/inicio/inicio.component.ts
+++ src/app/modules/pacientes/modules/index/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from 'src/app/services/current-user.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss'],
 })
 export class InicioComponent implements OnInit {
-  constructor() {}
+  public currentUser: any = {};
 
-  ngOnInit(): void {}
+  constructor(public currentUserService: CurrentUserService) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.currentUserService.currentUser;
+  }
 }
