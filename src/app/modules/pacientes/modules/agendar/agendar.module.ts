f69644d7f82ec44d674d74bf58e6866769016b61
Cristian Alarcon T <35607474+crisaty@users.noreply.github.com>
2020-09-16 20:03:29 -0300

    Add autocomplete to professional's flow for take an appointment

--- src/app/modules/pacientes/modules/agendar/agendar.module.ts
+++ src/app/modules/pacientes/modules/agendar/agendar.module.ts
@@ -3,28 +3,25 @@ import { CommonModule } from '@angular/common';
 import { AgendarRoutingModule } from './agendar-routing.module';
 import { IndexComponent } from './components/index/index.component';
 import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
-import { ResultadoComponent } from './components/resultado/resultado.component'
+import { ResultadoComponent } from './components/resultado/resultado.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SafePipe } from './../../../../shared/pipes/sanitizer.pipe';
 import { NgxSpinnerModule } from 'ngx-spinner';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    IndexComponent, 
-    ResultadoComponent, 
-    SafePipe
-  ],
+  declarations: [IndexComponent, ResultadoComponent, SafePipe],
   imports: [
     CommonModule,
     AgendarRoutingModule,
     NgbDatepickerModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
   ],
-  exports:[
-    SafePipe
-  ]
+  exports: [SafePipe],
 })
-export class AgendarModule { }
+export class AgendarModule {}
