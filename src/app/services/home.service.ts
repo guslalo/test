a1eeecfe9de0829a210a89c819cf72e9aff7e30f
guslalo <gf.renjifo@gmail.com>
2020-09-17 16:19:54 -0300

    jitsi + notas

---
+++ src/app/services/home.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeService {
+  private home = 'v1/home';
+  private tips = 'v1/tips';
+
+
+  private patients = 'v1/patient';
+  private photo = 'v1/account/profilePhoto';
+
+  constructor(private http: HttpClient) {}
+
+  // get data
+  getTips(): Observable<any> {
+    return this.http.get<any>(environment.baseUrl + this.tips )
+  }
+
+  // get data
+  /*
+  updateUser(userData): Observable<any> {
+    console.log(userData);
+    return this.http.put<any>(environment.baseUrl + this.account, userData, {
+      headers: new HttpHeaders({ Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token')) }),
+    });
+  }*/
+
+}
