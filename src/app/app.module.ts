912ceb1db15c65450cbcd6cb99fad8ffac5d22c9
Mikel Carozzi <cadrogui@gmail.com>
2020-09-21 13:03:57 -0300

    implmentacion de interceptor global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,11 @@ import { AngularFireMessagingModule } from '@angular/fire/messaging';
 import { AsyncPipe } from '@angular/common';
 import { environment } from './../environments/environment';
 import { FileUtilsService } from './services/file-utils.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpToastrInterceptor } from './interceptors/http-toastr.interceptor';
+import { ToastrModule } from 'ngx-toastr';
+
+const toastrConfig = { closeButton: true };
 
 @NgModule({
   declarations: [AppComponent, ErrorDialogComponent],
@@ -56,9 +61,23 @@ import { FileUtilsService } from './services/file-utils.service';
     AngularFirestoreModule, // For FireStore
     AngularFireStorageModule, // For Storage
     AngularFireAuthModule, // For Authentication
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    ToastrModule.forRoot(toastrConfig),
   ],
-  providers: [AngularFireDatabaseModule, ErrorDialogService, MessagingService, AsyncPipe, FileUtilsService],
+  providers: [
+    AngularFireDatabaseModule,
+    ErrorDialogService,
+    MessagingService,
+    AsyncPipe,
+    FileUtilsService,
+    // INTERCEPTOR TOASTR
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpToastrInterceptor,
+      multi: true,
+    },
+    // INTERCEPTOR TOASTR
+  ],
   bootstrap: [AppComponent],
   entryComponents: [ErrorDialogComponent],
 })
---
+++ src/app/interceptors/http-toastr.interceptor.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HttpResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable()
+export class HttpToastrInterceptor implements HttpInterceptor {
+
+  constructor(private toastr: ToastrService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): any {
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          if (event.status == 200) {
+            this.toastr.success('Http Status OK')
+          }
+        }
+        return event
+      }),
+      catchError((err: HttpErrorResponse) => {
+        if (err.status == 400 || err.status == 500) {
+          this.toastr.error(err.error.message);
+        }
+        return throwError(err)
+      })
+    );
+  }
+}
