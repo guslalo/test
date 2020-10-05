8e3986209362d7f10907fff265462e3ea05143f8
guslalo <gf.renjifo@gmail.com>
2020-10-05 11:42:24 -0300

    se remueve patrones para creacion de nombre en registro, el cual bloquea tipo de caracter

--- src/app/modules/auth/components/create-account/create-account.component.ts
+++ src/app/modules/auth/components/create-account/create-account.component.ts
@@ -94,10 +94,10 @@ export class CreateAccountComponent implements OnInit {
     this.personalData = this._formBuilder.group(
       {
         checkAge: [null, [Validators.requiredTrue]],
-        name: ['', [Validators.required, Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)]],
-        lastName: ['', Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)],
-        motherName: ['', [Validators.required, Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)]],
-        secondLastName: ['', [Validators.required, Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)]],
+        name: ['', [Validators.required, ]],//Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)
+        lastName: ['', ], //Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)
+        motherName: ['', [Validators.required, ]],//Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)
+        secondLastName: ['', [Validators.required, ]],//Validators.pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/)
         email: [null, [Validators.email, Validators.required]],
         gender: [null, [Validators.required]],
         confirmEmail: ['', [Validators.required]],
