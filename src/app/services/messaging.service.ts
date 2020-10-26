1ba70bef5514a5086055b318de6ba5805827394d
jose.veliz <jose.veliz@grupoleiros.com>
2020-10-26 11:59:36 -0400

    Fix on push notifications

--- src/app/services/messaging.service.ts
+++ src/app/services/messaging.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireMessaging } from '@angular/fire/messaging';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { mergeMapTo } from 'rxjs/operators';
+import { BehaviorSubject, Observable} from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root',
@@ -13,10 +11,10 @@ export class MessagingService {
   currentMessage = new BehaviorSubject(null);
 
   constructor(private angularFireMessaging: AngularFireMessaging, private http: HttpClient) {
-    this.angularFireMessaging.messaging.subscribe((_messaging) => {
-      _messaging.onMessage = _messaging.onMessage.bind(_messaging);
-      _messaging.onTokenRefresh = _messaging.onTokenRefresh.bind(_messaging);
-    });
+    // this.angularFireMessaging.messaging.subscribe((_messaging) => {
+    //   _messaging.onMessage = _messaging.onMessage.bind(_messaging);
+    //   _messaging.onTokenRefresh = _messaging.onTokenRefresh.bind(_messaging);
+    // });
   }
   requestPermission() {
     this.angularFireMessaging.requestToken.subscribe(
