b7e3296d2d0d782559ae7f24857e06c58d93fc59
guslalo <gf.renjifo@gmail.com>
2020-12-15 18:10:56 -0300

    boolean tutor

--- src/app/modules/profesionales/modules/crear-ficha/components/crear-ficha-consulta/crear-ficha-consulta.component.ts
+++ src/app/modules/profesionales/modules/crear-ficha/components/crear-ficha-consulta/crear-ficha-consulta.component.ts
@@ -515,6 +515,9 @@ export class CrearFichaConsultaComponent implements OnInit {
       })
 
       this.objectDiagnostic = item
+      this.arrayDiagnostic2 = [
+        this.objectDiagnostic
+      ]
       
       this.arrayDiagnostic.push({
         display: item.display,
@@ -1156,6 +1159,8 @@ export class CrearFichaConsultaComponent implements OnInit {
 
         this.otros.controls['physicalExam'].setValue(data.payload.appointmentDetails.physicalExam);
         this.otros.controls['examHighlights'].setValue(data.payload.appointmentDetails.examHighlights);
+
+
         /*
         if(
           data.payload.patientDetails.vitalSigns.PAS != null,
@@ -1175,6 +1180,8 @@ export class CrearFichaConsultaComponent implements OnInit {
         this.signos.controls['Sat'].setValue(data.payload.patientDetails.vitalSigns.Sat);
         }*/
 
+
+      
         this.signos.controls['PAS'].setValue(data.payload.patientDetails.vitalSigns.PAS);
         this.signos.controls['PAD'].setValue(data.payload.patientDetails.vitalSigns.PAD);
         this.signos.controls['PAmedia'].setValue(data.payload.patientDetails.vitalSigns.PAmedia);
