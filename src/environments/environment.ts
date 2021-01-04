700c1c7e0cdc8fe7346a8896c2e4cc0241b475f5
guslalo <gf.renjifo@gmail.com>
2021-01-04 15:31:21 -0300

    TCR-34

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  defaultLang: 'pt', //es, pt
+  defaultLang: 'es', //es, pt
   checkAge: false,
   title: 'Itms Dev | Teleconsulta',
-  baseUrl: 'https://backend-dev.bdot.app/api/',
+  baseUrl: 'https://backend-dev-cl.bdot.app/api/',
   client: 'itms',
   jitsi: 'meet.bdot.app', //meet.bdot.app
   apiKey: '',
