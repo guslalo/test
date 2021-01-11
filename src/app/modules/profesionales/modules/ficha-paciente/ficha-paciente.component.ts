8e9471631d3dcf3325af06ab3ae64699da52a3c4
guslalo <gf.renjifo@gmail.com>
2021-01-11 14:32:59 -0300

    imagen timeline

--- src/app/modules/profesionales/modules/ficha-paciente/ficha-paciente.component.ts
+++ src/app/modules/profesionales/modules/ficha-paciente/ficha-paciente.component.ts
@@ -34,6 +34,7 @@ export class FichaPacienteComponent implements OnInit {
   public antecedentes: any;
   public antecedentesGeneral: any;
 
+
   identificationData: any = {};
 
   // EXTRAS
@@ -61,6 +62,7 @@ export class FichaPacienteComponent implements OnInit {
   public descargar: boolean;
   public appointmentId: any;
   public urlSibrare: any;
+  public setup:any;
 
 
   constructor(
@@ -76,6 +78,7 @@ export class FichaPacienteComponent implements OnInit {
   tomorrow = new Date(2020, 9, 20, 14, 34);
 
   ngOnInit(): void {
+    this.setup = environment.setup
     console.log(this.userId);
     this.appointmentId = this.userId
     this.getMedicalRecord(this.userId);
