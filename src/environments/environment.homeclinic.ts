b7f41053c730accb20d3a7077f52cb5572a7375e
Mikel Carozzi <mikel@bluedott.tech>
2021-01-17 05:02:29 -0300

    new pipelines codedeploy, fixes

--- src/environments/environment.homeclinic.ts
+++ src/environments/environment.homeclinic.ts
@@ -3,7 +3,7 @@ export const environment = {
   defaultLang: 'pt',
   checkAge: false,
   title: 'Itms | Teleconsulta',
-  baseUrl: 'https://backend-testing-pt-br.bdot.app/api/',
+  baseUrl: 'https://backend-homeclinic.bdot.app/api/',
   // baseUrl: 'https://backend.homeclinic.telemedicina.com/api/',
   client: 'itms',
   jitsi: 'https://tc-regional-brasil.s3.us-east-1.amazonaws.com/', //meet.bdot.app
